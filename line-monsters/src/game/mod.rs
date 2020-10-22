f8bfe014e5bee9ee1e01bca65b6da0ff88309363
Ellen <supbscripter@gmail.com>
2020-10-22 23:34:06 +0100

    Proper de/ser support and map format

--- line-monsters/src/game/mod.rs
+++ line-monsters/src/game/mod.rs
@@ -1,13 +1,12 @@
 use crate::renderer::texture;
 use crate::renderer::State;
 use crate::renderer::{spritebatch::Spritebatch, Vertex};
+use nanoserde::{DeBin, SerBin};
 use std::sync::Arc;
 use wgpu::{Device, Queue};
 use winit::event::{ElementState, KeyboardInput, VirtualKeyCode, WindowEvent};
 
-mod map;
-
-#[derive(Copy, Clone, Debug)]
+#[derive(DeBin, SerBin, Copy, Clone, Debug)]
 pub enum Model {
     Wall,
     Corner,
@@ -36,19 +35,10 @@ impl Model {
             Model::InnerCorner => *self = Model::Wall,
         }
     }
-
-    fn serialize(&self) -> &'static str {
-        match &self {
-            Model::Wall => "Model::Wall",
-            Model::Corner => "Model::Corner",
-            Model::Floor => "Model::Floor",
-            Model::InnerCorner => "Model::InnerCorner",
-        }
-    }
 }
 
 /// Counter clockwise
-#[derive(Copy, Clone, Debug)]
+#[derive(DeBin, SerBin, Copy, Clone, Debug)]
 pub enum TileRotation {
     Zero,
     Quarter,
@@ -79,18 +69,9 @@ impl TileRotation {
             TileRotation::ThreeQuarters => *self = TileRotation::Zero,
         }
     }
-
-    fn serialize(&self) -> &'static str {
-        match &self {
-            TileRotation::Zero => "TileRotation::Zero",
-            TileRotation::Quarter => "TileRotation::Quarter",
-            TileRotation::Half => "TileRotation::Half",
-            TileRotation::ThreeQuarters => "TileRotation::ThreeQuarters",
-        }
-    }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(DeBin, SerBin, Copy, Clone, Debug)]
 pub struct Tile {
     height: u8,
     model: Model,
@@ -106,6 +87,50 @@ impl Tile {
         }
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct Chunk {
+    pub x: isize,
+    pub y: isize,
+    pub tiles: [Tile; Self::WIDTH * Self::HEIGHT],
+}
+
+impl DeBin for Chunk {
+    fn de_bin(o: &mut usize, d: &[u8]) -> std::result::Result<Self, nanoserde::DeBinErr> {
+        std::result::Result::Ok(Self {
+            x: DeBin::de_bin(o, d)?,
+            y: DeBin::de_bin(o, d)?,
+            tiles: DeBin::de_bin(o, d)?,
+        })
+    }
+}
+impl SerBin for Chunk {
+    fn ser_bin(&self, s: &mut Vec<u8>) {
+        self.x.ser_bin(s);
+        self.y.ser_bin(s);
+        self.tiles.ser_bin(s);
+    }
+}
+
+impl Chunk {
+    const WIDTH: usize = 32;
+    const HEIGHT: usize = 32;
+
+    const I_WIDTH: isize = 32;
+    const I_HEIGHT: isize = 32;
+
+    fn new(x: isize, y: isize) -> Self {
+        Self {
+            x,
+            y,
+            tiles: [Tile::new_rotation(0, Model::Floor, TileRotation::Zero);
+                Self::WIDTH * Self::HEIGHT],
+        }
+    }
+}
+
+#[derive(DeBin, SerBin)]
+pub struct Map(Vec<Chunk>);
 
 pub struct Scene {
     spritebatch: Spritebatch,
@@ -126,8 +151,9 @@ pub struct Scene {
 
     rotate_tile: bool,
 
-    selected: (u8, u8),
-    map: [[Tile; 16]; 12],
+    selected: (isize, isize),
+
+    chunks: Map,
 }
 
 impl Scene {
@@ -141,9 +167,9 @@ impl Scene {
         let ground_wall_texture =
             texture::Texture::from_bytes(&device, &queue, ground_wall_bytes, "ground-wall.png");
 
-        let map = map::map();
+        let chunks = Vec::new();
 
-        Self {
+        let mut scene = Self {
             spritebatch,
             grass_texture,
             ground_wall_texture,
@@ -163,8 +189,43 @@ impl Scene {
             rotate_tile: false,
 
             selected: (0, 0),
-            map,
-        }
+
+            chunks: Map(chunks),
+        };
+
+        scene.deserialize();
+
+        scene
+    }
+
+    pub fn serialize(&self) {
+        use std::fs::File;
+        use std::io::prelude::*;
+        use std::path::Path;
+
+        let bytes = SerBin::serialize_bin(&self.chunks);
+
+        let path = Path::new("map_data.blob");
+
+        let mut file = File::create(&path).unwrap();
+
+        file.write_all(&bytes).unwrap();
+    }
+
+    pub fn deserialize(&mut self) {
+        use std::fs::File;
+        use std::io::prelude::*;
+        use std::path::Path;
+
+        let path = Path::new("map_data.blob");
+
+        let mut file = File::open(&path).unwrap();
+
+        let mut data = Vec::with_capacity(std::mem::size_of::<Chunk>());
+        file.read_to_end(&mut data).unwrap();
+
+        let map: Map = DeBin::deserialize_bin(&data).unwrap();
+        self.chunks = map;
     }
 
     pub fn input(&mut self, event: &WindowEvent) -> bool {
@@ -223,48 +284,51 @@ impl Scene {
         }
     }
 
-    fn serialize_map(&self) {
-        let create_tile_str = |height: u8, model: Model, rotation: TileRotation| {
-            format!(
-                "Tile::new_rotation({}, {}, {}),",
-                height.to_string(),
-                model.serialize(),
-                rotation.serialize()
-            )
+    pub fn xy_to_chunk_coord(x: isize, y: isize) -> (isize, isize, usize, usize) {
+        let chunk_x = f32::floor(x as f32 / 32.0) as isize;
+        let chunk_y = f32::floor(y as f32 / 32.0) as isize;
+
+        let (width, height) = (Chunk::I_WIDTH, Chunk::I_HEIGHT);
+        let x_pos = if x >= 0 {
+            x % width
+        } else {
+            width - 1 - isize::abs((x + 1) % width)
+        };
+        let y_pos = if y >= 0 {
+            y % height
+        } else {
+            height - 1 - isize::abs((y + 1) % height)
         };
 
-        let create_row_str = |row: &[Tile]| {
-            let mut row_string = "[".to_owned();
-            for tile in row.iter() {
-                let tile_str = create_tile_str(tile.height, tile.model, tile.rotation);
-                row_string.push_str(&tile_str);
-            }
-            row_string.push_str("],");
+        assert!(x_pos >= 0 && y_pos >= 0);
 
-            row_string
-        };
+        (chunk_x, chunk_y, x_pos as usize, y_pos as usize)
+    }
 
-        let mut map_str = "let map = [".to_owned();
-        for row in self.map.iter() {
-            map_str.push_str(&create_row_str(row));
-        }
-        map_str.push_str("];");
+    pub fn get_tile(&mut self, x: isize, y: isize) -> Option<&mut Tile> {
+        let (chunk_x, chunk_y, x, y) = Self::xy_to_chunk_coord(x, y);
 
-        println!("{}", map_str);
+        let chunk = self
+            .chunks
+            .0
+            .iter_mut()
+            .find(|chunk| chunk.x == chunk_x && chunk.y == chunk_y)?;
+
+        Some(&mut chunk.tiles[(y * 32 + x) as usize])
     }
 
     pub fn tick(&mut self, state: &mut State) {
-        if self.move_select_up && self.selected.1 > 0 {
+        if self.move_select_up {
             self.selected.1 -= 1;
         }
-        if self.move_select_down && self.selected.1 < 11 {
+        if self.move_select_down {
             self.selected.1 += 1;
         }
 
-        if self.move_select_left && self.selected.0 > 0 {
+        if self.move_select_left {
             self.selected.0 -= 1;
         }
-        if self.move_select_right && self.selected.0 < 15 {
+        if self.move_select_right {
             self.selected.0 += 1;
         }
         self.move_select_up = false;
@@ -273,21 +337,23 @@ impl Scene {
         self.move_select_right = false;
 
         if self.save_map_return {
-            self.serialize_map();
+            self.serialize();
         }
         self.save_map_return = false;
 
         if self.raise {
-            let tile = &mut (&mut self.map[self.selected.1 as usize])[self.selected.0 as usize];
-            if tile.height != u8::MAX {
-                tile.height += 1;
+            if let Some(tile) = self.get_tile(self.selected.0, self.selected.1) {
+                if tile.height != u8::MAX {
+                    tile.height += 1;
+                }
             }
         }
 
         if self.lower {
-            let tile = &mut (&mut self.map[self.selected.1 as usize])[self.selected.0 as usize];
-            if tile.height > 0 {
-                tile.height -= 1;
+            if let Some(tile) = self.get_tile(self.selected.0, self.selected.1) {
+                if tile.height > 0 {
+                    tile.height -= 1;
+                }
             }
         }
 
@@ -295,22 +361,24 @@ impl Scene {
         self.lower = false;
 
         if self.swap_model {
-            let tile = &mut (&mut self.map[self.selected.1 as usize])[self.selected.0 as usize];
-            tile.model.swap_model();
+            if let Some(tile) = self.get_tile(self.selected.0, self.selected.1) {
+                tile.model.swap_model();
+            }
         }
         self.swap_model = false;
 
         if self.rotate_tile {
-            let tile = &mut (&mut self.map[self.selected.1 as usize])[self.selected.0 as usize];
-            tile.rotation.rotate_next();
+            if let Some(tile) = self.get_tile(self.selected.0, self.selected.1) {
+                tile.rotation.rotate_next();
+            }
         }
         self.rotate_tile = false;
 
         fn produce_verts(
             tile: &Tile,
-            (x, y): (usize, usize),
+            (x, y): (isize, isize),
             rotation: TileRotation,
-            selected: (u8, u8),
+            (selected_x, selected_y): (isize, isize),
             model: Model,
         ) -> (Vec<Vertex>, &'static [u16]) {
             let (vertices, indices) = model.get_model();
@@ -321,7 +389,7 @@ impl Scene {
                     position: [
                         vertex.position[0] + x as f32,
                         vertex.position[1] + tile.height as f32 + {
-                            if selected.0 as usize == x && selected.1 as usize == y {
+                            if selected_x == x && selected_y == y {
                                 0.25
                             } else {
                                 0.0
@@ -331,60 +399,66 @@ impl Scene {
                     ],
                     tex_coords: vertex.tex_coords,
                 })
-                /*.map(|vertex| Vertex {
-                    position: [
-                        vertex.position[0],
-                        vertex.position[1],
-                        vertex.position[2] * 1.25,
-                    ],
-                    tex_coords: vertex.tex_coords,
-                })*/
                 .collect();
             (vertices, indices)
         }
 
-        for (y, row_data) in self.map.iter().enumerate() {
-            for (x, tile) in row_data.iter().enumerate().filter(|(_, tile)| {
-                if let Model::Floor = tile.model {
-                    true
-                } else {
-                    false
-                }
-            }) {
+        for chunk in self.chunks.0.iter() {
+            let map = &chunk.tiles;
+
+            for (x, y, tile) in map
+                .iter()
+                .enumerate()
+                .filter(|(_, tile)| {
+                    if let Model::Floor = tile.model {
+                        true
+                    } else {
+                        if let Model::Corner = tile.model {
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                })
+                .map(|(n, tile)| {
+                    (
+                        (n as isize % Chunk::I_WIDTH) + chunk.x * Chunk::I_WIDTH, // x
+                        (n as isize / Chunk::I_HEIGHT) + chunk.y * Chunk::I_HEIGHT, // y
+                        tile,
+                    )
+                })
+            {
                 let (vertices, indices) =
                     produce_verts(tile, (x, y), tile.rotation, self.selected, Model::Floor);
                 let texture = self.grass_texture.clone();
 
                 self.spritebatch.push_verts(&vertices, indices, texture);
             }
-        }
 
-        for (y, row_data) in self.map.iter().enumerate() {
-            for (x, tile) in row_data.iter().enumerate().filter(|(_, tile)| {
-                if let Model::Floor = tile.model {
-                    false
-                } else {
-                    true
-                }
-            }) {
+            for (x, y, tile) in map
+                .iter()
+                .enumerate()
+                .filter(|(_, tile)| {
+                    if let Model::Floor = tile.model {
+                        false
+                    } else {
+                        true
+                    }
+                })
+                .map(|(n, tile)| {
+                    (
+                        (n as isize % Chunk::I_WIDTH) + chunk.x * Chunk::I_WIDTH, // x
+                        (n as isize / Chunk::I_HEIGHT) + chunk.y * Chunk::I_HEIGHT, // y
+                        tile,
+                    )
+                })
+            {
                 let (vertices, indices) =
                     produce_verts(tile, (x, y), tile.rotation, self.selected, tile.model);
 
                 let texture = self.ground_wall_texture.clone();
 
                 self.spritebatch.push_verts(&vertices, indices, texture);
-
-                if let Model::Corner = tile.model {
-                    let (vertices, indices) = produce_verts(
-                        tile,
-                        (x, y),
-                        TileRotation::Zero,
-                        self.selected,
-                        Model::Floor,
-                    );
-                    let texture = self.grass_texture.clone();
-                    self.spritebatch.push_verts(&vertices, &indices, texture);
-                }
             }
         }
 
